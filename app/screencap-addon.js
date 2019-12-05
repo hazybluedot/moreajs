01e776632b520c16492a19045ebe696a73faafe7
Darren Maczka <dkm@utk.edu>
2019-12-05 18:48:07 -0500

    fix typo

--- app/screencap-addon.js
+++ app/screencap-addon.js
@@ -1,4 +1,4 @@
-module.export = (idx, el) => {
+module.exports = (idx, el) => {
   var currentTitle = $(el).attr('title');
   //console.log('adding screencap expand to image: ' + $(el).attr('src'));
   $(el).attr('title', '(click to enlarge)');
