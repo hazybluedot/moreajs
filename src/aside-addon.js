6b79a1f09dfeb926a182b38f89e5bfc9bf30274b
Darren Maczka <dkm@utk.edu>
2020-01-25 17:14:03 -0500

    only add row class to section if section contains asides

--- src/aside-addon.js
+++ src/aside-addon.js
@@ -1,16 +1,15 @@
 export default (idx, el) => {
-    let $sec = $(el);
-    if ($sec.children('section').length == 0) {
-	console.log('applying aside addon to section', $sec);
-	//$sec.addClass("row");  
-	let content = $sec.children('.content').first();
+  let $sec = $(el);
+  if ($sec.children('section').length == 0) {
+	let content = $sec.children('.content').length > 0 ? $sec.children('.content').first() : $sec;
 	let asides = content.children('aside');
 	if (asides.length > 0) {
-	    content.addClass("row");
-	    let main = content.children(':not(aside)');
-	    
-	    main.wrapAll($('<div />', {"class": "col-md-6"}));
-	    asides.wrapAll($('<div />', {"class": "col-md-3"}));
+      //console.log('putting aside in a column', content, asides);
+	  content.addClass("row");
+	  let main = content.children(':not(aside)');
+	  
+	  main.wrapAll($('<div />', {"class": "col-md-6"}));
+	  asides.wrapAll($('<div />', {"class": "col-md-3"}));
 	}
-    }
+  }
 };
