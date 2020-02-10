7c5fb4e2da4f8bc2cdf549b14233d90826b2c1a5
Darren Maczka <dkm@utk.edu>
2020-02-10 17:24:57 -0500

    bring click handler into package

--- src/imgswap-addon.js
+++ src/imgswap-addon.js
@@ -1,6 +1,21 @@
+function swapimage(e) {
+  let i = e.target;
+  var s=i.src;
+  var len=s.length;
+  var post="-alt";
+  var dot=s.lastIndexOf('.');
+  var tlen=post.length;
+  if (s.substr(dot-tlen,tlen)==post) {
+    i.src = s.substr(0,dot-tlen) + s.substr(dot);
+  }
+  else {
+    i.src = s.substr(0,dot) + post + s.substr(dot);
+  }
+}
+
 module.exports = function(idx, el) {
   const src = el.getAttribute('src').split('\.')
   if (src[0].endsWith('-alt')) {
-    el.setAttribute('onclick', 'swapimage(this)')
+    el.onclick =  swapimage;
   }
 };
